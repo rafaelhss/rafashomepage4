30eee40d3c9ac586f06f679d406da2857379f2d6
Rafaelhss <rafaelhss@gmail.com>
2018-11-05 14:30:19 -0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,4 @@
+    <meta property="og:image" content="http://campusparty.herokuapp.com/img/negao.png"/>
+    <meta property="og:title" content="Campus Party 2017"/>
+    <meta property="og:description" content="Quero uma camiseta"/>
+<?php include_once("home.html"); ?>
